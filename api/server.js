7c72ef3e9fb1defe1442e4ec8922d8a7f924fff6
Anthony Kawa <anthonyk2020@gmail.com>
2020-04-10 10:58:04 -0700

    attempted stretch

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
 const actionRouter = require('./Actions/router');
 const projectRouter = require('./Projects/router');
 
+server.use(cors());
 server.use(express.json());
 server.use('/api/actions', actionRouter);
 server.use('/api/projects', projectRouter);
