1cc88df27406552877c48f93d87802a9e2a38494
Anthony Kawa <anthonyk2020@gmail.com>
2020-04-10 08:57:33 -0700

    added loggers, express, start script, and server script

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+require('dotenv').config();
+const express = require('express');
+const server = require('./api/server');
+const morgan = require('morgan');
+const logger = require('./logger');
 
-Sing along:
+server.use(morgan("combined", { stream: logger.stream }))
+server.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+server.get('/', (req, res, next) => {
+    res.status(200).json(req.originalUrl);
+})
 
-Go code!
-*/
+server.listen(PORT, () => {
+    logger.info(`Listening on port: ${PORT}`)
+})
